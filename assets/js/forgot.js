e2853ea718021aed8633b6a5b1c607f02367f6f1
Shalom-source <alasa@up.edu.ph>
2025-12-07 22:01:00 +0800

    chore: added forgot password page with html and js file

---
+++ assets/js/forgot.js
@@ -0,0 +1,28 @@
+const startingMinutes = 5;
+let time = startingMinutes * 60;
+const resetBtn = document.getElementById('reset');
+resetBtn.disabled = true;
+const countdownEl = document.getElementById('countdown');
+const countdownInterval = setInterval(updateCountdown, 1000);
+
+function updateCountdown(){
+    if (time < 0) {
+        resetBtn.disabled = false;
+        clearInterval(countdownInterval);
+        countdownEl.innerHTML = "00:00";
+        return;
+    }
+    resetBtn.disabled = true;
+    const minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+    countdownEl.innerHTML = `${minutes}:${seconds}`;
+    time--;
+}
+
+document.getElementById('reset').addEventListener('click', function() {
+time = startingMinutes * 60;
+updateCountdown();
+clearInterval(countdownInterval);
+countdownInterval = setInterval(updateCountdown, 1000);
+});
